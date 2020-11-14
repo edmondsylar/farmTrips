7f4cedc26460ed75f43be8462bdcf19a7fbaf1bd
edmondsylar <edmondmusiitwa@gmail.com>
2020-11-14 08:38:16 -0800

    trip start stop

--- resources/views/trips.blade.php
+++ resources/views/trips.blade.php
@@ -7,19 +7,29 @@
         </div>
     </div>
 
-<div class="row">
-    <div class="col-md-3 col-sm-6 col-6 equel-grid">
-        <div class="grid">
-            <div class="grid-body text-gray">
-                <div class="d-flex justify-content-between">
-                    <p> Destination </p>
-                    <span class="badge badge-success">Available</span>
-                </div>
-                <p class="text-black">Group Name</p>
-                <div class="wrapper w-50 mt-4">
-                    
+    <div class="row">
+        <div class="col-md-3 col-sm-6 col-6 equel-grid">
+            <div class="grid">
+                <div class="grid-body text-gray">
+                    <div class="d-flex justify-content-between">
+                        <p> Destination {{ $request->Destination }} </p>
+                        <span class="badge badge-success"></span>
+                    </div>
+                <p class="text-black">Group Name : {{ $request->group }} </p>
+                    <div class="wrapper w-50 mt-4">
+                        
+
+                        <button class="btn btn-xs btn-inverse-primary">
+                            Start
+                        </button>
+
+                        <button class="btn btn-xs btn-inverse-stop">
+                            Stop
+                        </button>
+
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+@endsection
