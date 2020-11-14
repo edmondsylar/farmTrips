3208d64ee0f5d0482d118edc5cd21caf94cc037b
edmondsylar <edmondmusiitwa@gmail.com>
2020-11-14 04:11:15 -0800

    group stats

--- resources/views/group_join.blade.php
+++ resources/views/group_join.blade.php
@@ -36,7 +36,7 @@
             <div class="grid">
                 <div class="grid-body">
                   <div class="split-header">
-                    <p class="card-title">Activity Log</p>
+                    <p class="card-title">Group Members</p>
                     <div class="btn-group">
                       <button type="button" class="btn btn-trasnparent action-btn btn-xs component-flat pr-0" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <i class="mdi mdi-dots-vertical"></i>
@@ -49,42 +49,14 @@
                   </div>
                   <div class="vertical-timeline-wrapper">
                     <div class="timeline-vertical dashboard-timeline">
-                      <div class="activity-log">
-                        <p class="log-name">Agnes Holt</p>
-                        <div class="log-details">Analytics dashboard has been created<span class="text-primary ml-1">#Slack</span></div>
-                        <small class="log-time">8 mins Ago</small>
-                      </div>
-                      <div class="activity-log">
-                        <p class="log-name">Ronald Edwards</p>
-                        <div class="log-details">Report has been updated <div class="grouped-images mt-2">
-                            <img class="img-sm" src="../assets/images/profile/male/image_4.png" alt="Profile Image" data-toggle="tooltip" title="Gerald Pierce">
-                            <img class="img-sm" src="../assets/images/profile/male/image_5.png" alt="Profile Image" data-toggle="tooltip" title="Edward Wilson">
-                            <img class="img-sm" src="../assets/images/profile/female/image_6.png" alt="Profile Image" data-toggle="tooltip" title="Billy Williams">
-                            <img class="img-sm" src="../assets/images/profile/male/image_6.png" alt="Profile Image" data-toggle="tooltip" title="Lelia Hampton">
-                            <span class="plus-text img-sm">+3</span>
-                          </div>
-                        </div>
-                        <small class="log-time">3 Hours Ago</small>
-                      </div>
-                      <div class="activity-log">
-                        <p class="log-name">Charlie Newton</p>
-                        <div class="log-details"> Approved your request <div class="wrapper mt-2">
-                            <button type="button" class="btn btn-xs btn-primary">Approve</button>
-                            <button type="button" class="btn btn-xs btn-inverse-primary">Reject</button>
-                          </div>
-                        </div>
-                        <small class="log-time">2 Hours Ago</small>
-                      </div>
-                      <div class="activity-log">
-                        <p class="log-name">Gussie Page</p>
-                        <div class="log-details">Added new task: Slack home page</div>
-                        <small class="log-time">4 Hours Ago</small>
-                      </div>
+                      
+                      
                       <div class="activity-log">
                         <p class="log-name">Ina Mendoza</p>
                         <div class="log-details">Added new images</div>
                         <small class="log-time">8 Hours Ago</small>
                       </div>
+                      
                     </div>
                   </div>
                 </div>
